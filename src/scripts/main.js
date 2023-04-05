59bbd633652b07e154e2dc6bb8662aa5ad394b09
Andrey Lehendzevych <andrey.lehendzevych@gmail.com>
2023-04-05 18:03:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listItems = list.querySelectorAll('li');
+
+listItems.forEach(item => {
+  if (item.children.length) {
+    const textNode = item.firstChild;
+    const spanTitle = document.createElement('span');
+
+    textNode.before(spanTitle);
+    spanTitle.prepend(textNode);
+  }
+});
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('span');
+
+  if (!item) {
+    return;
+  }
+
+  item.nextElementSibling.toggleAttribute('hidden');
+});
